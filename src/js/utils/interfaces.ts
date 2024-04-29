fb8642432fedef8fc3833c36ccbdc6fe0b6700ed
Saxon Keegan <17565852+SaxonK@users.noreply.github.com>
2024-04-29 13:04:49 +0100

    Update function for game states added. Various interfaces have been added for future classes and properties.

--- src/js/utils/interfaces.ts
+++ src/js/utils/interfaces.ts
@@ -1,9 +1,3 @@
-export interface Scope {
-  animationFrameId: number;
-  displayFramerate: boolean;
-  framerate: number;
-  state: Object;
-};
 export interface Cycle {
   frameCount: number;
   startTime: EpochTimeStamp;
@@ -11,4 +5,46 @@ export interface Cycle {
 };
 export interface Cycles {
   [key: string]: Cycle;
+};
+export interface Entity {
+  id: number;
+  type: string;
+  stats: Object;
+  state: State;
+};
+export interface Item {
+  id: number;
+  type: string;
+  stats: Object;
+};
+export interface Player {
+  damage: number;
+  hitpoints: number;
+  weapons: Array<Weapon>;
+  items: Array<Item>;
+};
+export interface State {
+  coins?: number;
+  hitpoints: number;
+  position: {
+    x: number;
+    y: number;
+  };
+  speed: number;
+  update: () => void;
+};
+export interface States {
+  entities: Array<Entity>;
+  player: State;
+};
+export interface Scope {
+  animationFrameId: number;
+  displayFramerate: boolean;
+  framerate: number;
+  state: States;
+};
+export interface Weapon {
+  id: number;
+  type: string;
+  stats: Object;
 };
